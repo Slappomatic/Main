b1f23119d0d1c4adac1e0350bd2ff669fa2f0964
Slappomatic <32641549+Slappomatic@users.noreply.github.com>
2017-10-09 09:28:47 -0400

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,15 @@
+import media
+import fresh_tomatoes
+
+
+""" Opens the file Movies.txt and sends it through a for loop to populate the
+    list movies and then opens the movies_page """
+movies= []
+f = open(r"movies.txt")
+for line in f:
+    fields = line.split("|")
+    movies.append(media.Movie(fields[0],fields[1],fields[2],fields[3]))
+
+fresh_tomatoes.open_movies_page(movies)
+
+
